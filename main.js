be7a7a0616966b53ac90fc89cb0d8253bdd3b263
SachinBartaula <sachinbartaula3@gmail.com>
2025-09-05 09:00:37 +0545

    [update]

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
 let datetime= new   Date();
-let date_time=datetime.toDateString;
+let date_time=datetime.toDateString();
 document.getElementById("time").innerHTML=(date_time);
 }
